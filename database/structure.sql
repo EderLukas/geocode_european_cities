1b63d99990ff36bf7f78529d024a149b8847977f
Lukas Eder <eder.lukas@bluewin.ch>
2019-08-10 18:58:59 +0200

    Added resource files, the database structure and the insert sql scripts.

---
+++ database/structure.sql
@@ -0,0 +1,13 @@
+CREATE TABLE country (
+  country_id SERIAL PRIMARY KEY,
+  country_name VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE city (
+  city_id SERIAL PRIMARY KEY,
+  country_id INTEGER NOT NULL REFERENCES country(country_id),
+  city_name VARCHAR(200) NOT NULL,
+  city_population NUMERIC NOT NULL,
+  city_latitude NUMERIC NOT NULL,
+  city_longitude NUMERIC NOT NULL
+);
