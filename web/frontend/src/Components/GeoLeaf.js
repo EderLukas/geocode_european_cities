e06d94cc8682e7f82a1ba659e92e0f988c148ed1
Lukas Eder <eder.lukas@bluewin.ch>
2019-08-10 21:24:05 +0200

    further impl of frontend.

--- web/frontend/src/Components/GeoLeaf.js
+++ web/frontend/src/Components/GeoLeaf.js
@@ -7,7 +7,9 @@ class GeoLeaf extends Component {
     super(props)
     this.state = {
       cityList: [],
-      cityListError: false
+      cityListError: false,
+      defaultLocation: [46.946664, 7.444244],
+      isDefaultlocation: true
     }
 
     this.getCityList = this.getCityList.bind(this)
@@ -15,7 +17,7 @@ class GeoLeaf extends Component {
   }
 
   getCityList () {
-    axios.get('http://localhost:4000/cityList')
+    axios.get('http://localhost:4000/Controller/cityListC.php')
       .then(res => {
         if (res !== 'error'){
           this.setState({ cityList: res.data })
@@ -33,13 +35,13 @@ class GeoLeaf extends Component {
   render() {
     const map = (
       // fitBounds={this.state.locations.map(e => [e.x_position, e.y_position])}
-      <Map center={[47.424422, 8.304614]}
+      <Map center={this.state.defaultLocation}
        zoom={13}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
         />
-        <Marker position={[47.424422, 8.304614]}>
+        <Marker position={this.state.isDefaultlocation ? this.state.defaultLocation : null}>
           <Popup>
               City: x<br/>
               Country: y<br/>
@@ -49,15 +51,19 @@ class GeoLeaf extends Component {
         </Marker>
       </Map>
     )
-    return (
-      this.state.cityListError ? 
-        <div className="request_error">
-          <p>'Request failed. Please try again.'</p>
-        </div> :
-        <div className='leaflet_map'>
-          { map }
-        </div>
-    )
+    return <div className='buttons_and_map'>
+      <button onClick={this.sendRequest}>Show cities</button>
+      <button onClick={this.setDefaultLocation}>Set default location</button>
+      {
+        this.state.cityListError ? 
+          <div className="request_error">
+            <p>'Request failed. Please try again.'</p>
+          </div> :
+          <div className='leaflet_map'>
+            { map }
+          </div>
+      }
+    </div>
   }
 }
 
