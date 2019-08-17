ec9f048b42660ad14e7b4033cd1aa1c7f053a5e6
Lukas Eder <eder.lukas@bluewin.ch>
2019-08-17 20:32:11 +0200

    Added frontend default map and cities map.

--- web/frontend/src/Components/GeoLeaf.js
+++ web/frontend/src/Components/GeoLeaf.js
@@ -8,19 +8,19 @@ class GeoLeaf extends Component {
     this.state = {
       cityList: [],
       cityListError: false,
-      defaultLocation: [46.946664, 7.444244],
-      isDefaultlocation: true
+      showDefaultLocation: true
     }
 
     this.getCityList = this.getCityList.bind(this)
     this.sendRequest = this.sendRequest.bind(this)
+    this.showDefaultLocation = this.showDefaultLocation.bind(this)
   }
 
   getCityList () {
     axios.get('http://localhost:4000/Controller/cityListC.php')
       .then(res => {
         if (res !== 'error'){
-          this.setState({ cityList: res.data })
+          this.setState({ showDefaultLocation: false, cityList: res.data })
         } else {
           this.setState({ cityListError: true })
         }
@@ -32,35 +32,63 @@ class GeoLeaf extends Component {
     this.getCityList()
   }
 
+  showDefaultLocation() {
+    this.setState({ showDefaultLocation: true })
+  }
+
   render() {
+    const default_position = [46.946664, 7.444244]
+    const default_city_name = 'Bern'
+    const default_country_name = 'Switzerland'
+    const default_city_population = 133115
+    const default_map = (
+      <Map center={default_position} zoom={12}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"/>
+        <Marker position={default_position} >
+          <Popup>
+            City: {default_city_name}<br/>
+            Country: {default_country_name}<br/>
+            Population: {default_city_population}
+          </Popup>
+        </Marker>
+      </Map>
+    )
+
     const map = (
-      // fitBounds={this.state.locations.map(e => [e.x_position, e.y_position])}
-      <Map center={this.state.defaultLocation}
-       zoom={13}>
+      <Map bounds={this.state.cityList.map(e => [e.city_latitude, e.city_longitude])}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
         />
-        <Marker position={this.state.isDefaultlocation ? this.state.defaultLocation : null}>
-          <Popup>
-              City: x<br/>
-              Country: y<br/>
-              Population: 42
-          </Popup>
-          <Circle center={[47.424422, 8.304614]} radius={200} fillColor={"blue"}/>
-        </Marker>
+        {
+          this.state.cityList.map((e) => {
+            return <Marker position={[e.city_latitude, e.city_longitude]}
+              key={e.city_name + '-' + e.city_latitude}>
+            <Popup>
+                City: {e.city_name}<br/>
+                Country: {e.country_name}<br/>
+                Population: {e.city_population}
+            </Popup>
+            <Circle center={[e.city_latitude, e.city_longitude]} radius={e.radius}
+             fillColor={"blue"}/>
+          </Marker>
+          })
+        }
       </Map>
     )
+
     return <div className='buttons_and_map'>
       <button onClick={this.sendRequest}>Show cities</button>
-      <button onClick={this.setDefaultLocation}>Set default location</button>
+      <button onClick={this.showDefaultLocation}>Show default location</button>
       {
         this.state.cityListError ? 
           <div className="request_error">
             <p>'Request failed. Please try again.'</p>
           </div> :
           <div className='leaflet_map'>
-            { map }
+            { this.state.showDefaultLocation ? default_map : map }
           </div>
       }
     </div>
